2cb1f2ca053481f0eed22f2f22b5f57d39a845f9
Kerim Karaman <55785875+kerimkaraman@users.noreply.github.com>
2025-04-12 16:11:53 +0300

    Update player.sh

--- custom_modules/player.sh
+++ custom_modules/player.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
 if ! playerctl -l | grep -q .; then
-  echo "⏹️ Hiçbir şey çalmıyor"
+  echo "⏹️ Player is empty"
   exit 0
 fi
 
 status=$(playerctl status 2>/dev/null)
 
 if [[ "$status" == "Stopped" ]]; then
-  echo "⏹️ Hiçbir şey çalmıyor"
+  echo "⏹️ Player is stopped"
   exit 0
 fi
 
 artist=$(playerctl metadata artist)
 title=$(playerctl metadata title)
+
+maxlen=40
+
+text="$artist - $title"
+
+if [ ${#text} -gt $maxlen ]; then
+  text="${text:0:$((maxlen - 3))}..."
+fi
 
 icon=""
 if [[ "$status" == "Playing" ]]; then
@@ -22,4 +30,4 @@ elif [[ "$status" == "Paused" ]]; then
   icon="⏸️"
 fi
 
-echo "$icon $artist - $title"
+echo "$icon $text"
